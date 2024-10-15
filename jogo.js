07ae9b95f1e9032ff352caf6554b4b5ad88cc649
Paola Medeiros <71988644+paola-medeiros@users.noreply.github.com>
2024-10-15 09:30:04 -0300

    cronometro e configuração da página de vitória

--- jogo.js
+++ jogo.js
@@ -1,6 +1,7 @@
 var altura = 0
 var largura = 0
 var vidas = 1
+var tempo = 15
 
 function ajustaTamanhoPalcoJogo(){
     altura = window.innerHeight
@@ -11,6 +12,20 @@ function ajustaTamanhoPalcoJogo(){
 
 ajustaTamanhoPalcoJogo()
 
+var cronometro = setInterval(function() {
+
+    tempo -=1
+
+    if(tempo < 0) {
+        clearInterval(cronometro)
+        clearInterval(criaMosquito)
+        window.location.href = 'vitoria.html'
+    } else {
+        document.getElementById('cronometro').innerHTML = tempo
+    }
+    
+},1000)
+
 function posicaoRandomica() {
 
     //remover o mosquito anterior (caso exista)
@@ -19,8 +34,8 @@ function posicaoRandomica() {
 
         if(vidas>3) {
 
-            alert('Interromper o jogo (game over)')
-            
+            window.location.href = 'fim-de-jogo.html'
+
         } else {
 
             document.getElementById('v' + vidas).src="imagens/coracao_vazio.png"
