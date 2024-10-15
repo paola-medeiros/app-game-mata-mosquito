f03f2cf6d036fdfb1885c618c83eb14a1a973f39
Paola Medeiros <71988644+paola-medeiros@users.noreply.github.com>
2024-10-15 08:45:14 -0300

    controle de vidas no jogo

--- jogo.js
+++ jogo.js
@@ -1,5 +1,6 @@
 var altura = 0
 var largura = 0
+var vidas = 1
 
 function ajustaTamanhoPalcoJogo(){
     altura = window.innerHeight
@@ -15,6 +16,18 @@ function posicaoRandomica() {
     //remover o mosquito anterior (caso exista)
     if(document.getElementById('mosquito')) {
         document.getElementById('mosquito').remove()
+
+        if(vidas>3) {
+
+            alert('Interromper o jogo (game over)')
+            
+        } else {
+
+            document.getElementById('v' + vidas).src="imagens/coracao_vazio.png"
+
+            vidas++
+        }
+
     }
     
 
@@ -35,6 +48,9 @@ function posicaoRandomica() {
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
     mosquito.id = 'mosquito'
+    mosquito.onclick = function() {
+        this.remove()
+    }
 
 
     document.body.appendChild(mosquito)
